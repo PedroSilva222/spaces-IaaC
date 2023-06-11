0a6defcc9647a74dae4764cad5ef287daedfb574
PedroSpiet <tyroxbr@gmail.com>
2023-06-11 18:00:09 -0300

    IAM ROLE ATTACHMENT SQS AND DLQ

--- infraestructure/sqs/main.tf
+++ infraestructure/sqs/main.tf
@@ -3,7 +3,7 @@
 ## combines their policies into an application role.
 ##
 
-module "notifications_queue" {
+module "geolocation_notification" {
   source = "./modules/sqs"
   queue_name = var.sqs_name
   common_tags = var.common_tags
@@ -12,4 +12,25 @@ module "notifications_queue" {
   sqs_name = var.sqs_name
   sqs_receive_wait_time = var.sqs_receive_wait_time
   sqs_retention_period = var.sqs_retention_period
+}
+
+resource "aws_iam_role" "application_role" {
+  name = "ApplicationRole"
+
+  assume_role_policy = jsonencode({
+    "Version"   = "2012-10-17",
+    "Statement" = [
+      {
+        "Effect"    = "Allow"
+        "Action"    = "sts:AssumeRole"
+        "Principal" = { "Service" = "ec2.amazonaws.com" }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "application_role_geoloc_producer" {
+  role       = aws_iam_role.application_role.name
+  policy_arn = module.geolocation_notification.producer_policy_arn
 }
